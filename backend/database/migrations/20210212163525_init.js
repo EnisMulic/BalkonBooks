a7df058a8984b78e9bfc54557d9eb221bb5dce4c
Enis Mulić <enis.s.mulic@gmail.com>
2021-02-12 17:08:53 +0100

    Fix database migrations to default to uuid when creating users and authors

--- backend/database/migrations/20210212163525_init.js
+++ backend/database/migrations/20210212163525_init.js
@@ -2,17 +2,17 @@ exports.up = function (knex) {
     return knex.schema
         .createTable("book", (table) => {
             table.string("isbn", 13).notNullable().primary();
-            table.string("title", 255).notNullable();
+            table.string("title").notNullable();
             table.integer("pages").unsigned().notNullable();
             table.integer("published").unsigned().nullable();
-            table.string("image", 255);
+            table.string("image");
         })
         .createTable("author", (table) => {
-            table.uuid("id").primary();
-            table.string("firstName", 255).notNullable();
-            table.string("lastName", 225).notNullable();
+            table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
+            table.string("firstName").notNullable();
+            table.string("lastName").notNullable();
             table.date("dob").notNullable();
-            table.string("image", 255);
+            table.string("image");
         })
         .createTable("book_author", (table) => {
             table.string("isbn", 13).notNullable();
@@ -22,8 +22,8 @@ exports.up = function (knex) {
             table.foreign("author_id").references("id").inTable("author");
         })
         .createTable("user", (table) => {
-            table.uuid("id").primary();
-            table.string("email", 255).unique().notNullable();
+            table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
+            table.string("email").unique().notNullable();
             table.string("salt", 16).unique().notNullable();
             table.string("hash", 128).unique().notNullable();
         });
