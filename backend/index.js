1665499649e2628836b200e2b984a3668e2638d2
Enis Mulić <enis.s.mulic@gmail.com>
2021-02-14 15:52:29 +0100

    Add cors middleware

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
@@ -11,7 +12,7 @@ const apiRoute = require("./routes");
 dotenv.config();
 const port = process.env.PORT || 3001;
 const app = express();
-
+app.use(cors());
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
