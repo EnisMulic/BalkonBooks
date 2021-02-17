f1980803fac7dfa1baff452b48f6cbd7ca38326b
Enis Mulić <enis.s.mulic@gmail.com>
2021-02-17 20:39:36 +0100

    Update home page

--- frontend/src/containers/Home/index.js
+++ frontend/src/containers/Home/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import defaultBookImage from "../../assets/background.png";
+
+import style from "./Home.module.css";
+
 const Home = (props) => {
-    return <div>Home</div>;
+    return (
+        <div className={style.Home}>
+            <img src={defaultBookImage} />
+        </div>
+    );
 };
 
 export default Home;
